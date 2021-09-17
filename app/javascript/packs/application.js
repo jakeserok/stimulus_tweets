b9519a19d42446f9d80d7175881cc674da3ddb8f
jakeserok <jakeserok@gmail.com>
2021-09-16 22:47:51 -0400

    Application.js updated for stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+const application = Application.start();
+const controllers = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(controllers));
